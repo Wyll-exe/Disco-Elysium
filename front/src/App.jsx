8db288292bd7bba253b346f9308bed18db8067cc
Wyll <williamagossah@gmail.com>
2025-06-13 18:01:12 +0200

    ✨ videos

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,7 +7,19 @@ function App() {
 
   return (
     <>
-      aAAAA
+      <div className='flex items-center justify-center pt-[8rm]'>
+
+        <div className='absolute top-0 left-0 w-full min-h-screen'>
+          <video
+            autoPlay
+            loop
+            muted
+            className='w-full h-full object-cover fixed'>
+            <source src="/assets/videos/beach.mp4" type="video/mp4" />
+          </video>
+          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 pointer-events-none"></div>
+        </div>
+      </div>
     </>
   )
 }
